f3f888cdaa23c3e330cc518830da8e9ccd200fae
Zakcrust <zakcrusted@gmail.com>
2025-05-13 10:25:52 +0700

    add info

---
+++ main.ts
@@ -0,0 +1,7 @@
+function info() {
+    console.log("Nama  \t\t\t: Kamal");
+    console.log("Angkatan \t\t: 20xx");
+    console.log("NIM \t\t\t: 1177XXXXXX");
+    console.log("Grup ACA  \t\t: 13");
+}
+info();
